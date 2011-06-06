9203bfdff1bd1e5c7d82fe6fa99145a051e089a4
Rob Cabacungan <robert.cabacungan@teamaol.com>
2011-06-06 14:20:46 -0400

    Initial commit.

---
+++ project/myproj_sup.erl
@@ -0,0 +1,29 @@
+-module({{projectid}}_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 2000, Type, [I]}).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+init([]) ->
+    Children = [
+		?CHILD({{projectid}}_server, worker) 
+		],
+    {ok, { {one_for_one, 5, 10}, Children} }.
